7994eed4cd1b13a923d2107c3456c00a826aa641
Ariful Islam <arifulq234@gmail.com>
2025-09-04 10:13:04 +0600

    Completed word details with daisiUI Modal

--- script.js
+++ script.js
@@ -5,25 +5,78 @@ const loadLesson = () => {
     .then((data) => displayLesson(data.data));
 };
 
+const loadWordDetails = async (id) => {
+  const urlOfWordDetails = `https://openapi.programming-hero.com/api/word/${id}`;
+  const response = await fetch(urlOfWordDetails);
+  const data = await response.json();
+  displayWordDetails(data.data);
+};
+
+// id: 5;
+// level: 1;
+// meaning: "আগ্রহী";
+// partsOfSpeech: "adjective";
+// points: 1;
+// pronunciation: "ইগার";
+// sentence: "The kids were eager to open their gifts.";
+// synonyms: (3)[("enthusiastic", "excited", "keen")];
+// word: "Eager";
+const displayWordDetails = (datas) => {
+  const wordMOdalContainer = document.getElementById("my_modal_5");
+
+  wordMOdalContainer.innerHTML = ` 
+    
+<div class="modal-box space-y-3">
+    <div class="">
+        <h1 class="font-semibold text-3xl">(  <i class="fa-solid fa-microphone-lines"></i>   :${datas.pronunciation})</h1>
+    </div>
+    <div>
+        <h1 class="font-semibold text-2xl">Meaning</h1>
+        <p class="font-bangla font-medium text-2xl">${datas.meaning}</p>
+    </div>
+    <div>
+        <h1 class="font-semibold text-2xl">Example</h1>
+        <p class="font-bangla font-medium text-2xl">${datas.sentence}</p>
+    </div>
+    <div>
+        <h1 class="font-semibold font-bangla text-2xl">সমার্থক শব্দ গুলো</h1>
+        
+    </div>
+    <div>
+      <p class="btn">afafdfafa</p>        
+      <p class="btn">afafdfafa</p>        
+      <p class="btn">afafdfafa</p>        
+    </div>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+   `;
+  document.getElementById("my_modal_5").showModal();
+};
 // word load and word related other functionality here
+const lessongBtnremove = () => {
+  const lessonbtns = document.querySelectorAll(".lessongBtnRemove");
+  lessonbtns.forEach((lessonbtn) => {
+    lessonbtn.classList.remove("activeBtn");
+  });
+};
 
 const wordLoad = (id) => {
   fetch(`https://openapi.programming-hero.com/api/level/${id}`)
     .then((res) => res.json())
     .then((datas) => {
-        displayWord(datas.data);
-        const lessonBtn = document.getElementById(
-          `lessonbtn${id}`
-        );
-        const activeBtnremove =
-          lessonBtn.parentElement.parentElement.childNodes;
-        activeBtnremove.forEach(removeAcBtn=> {
-            removeAcBtn.classList.remove("activeBtn")
-        })
-        // activeBtnremove.classList.remove("activeBtn")
-       lessonBtn.classList.add("activeBtn");
+      displayWord(datas.data);
+      const lessonBtn = document.getElementById(`lessonbtn${id}`);
+
+      lessongBtnremove();
+      lessonBtn.classList.add("activeBtn");
     });
 };
+
 // id: 90;
 // level: 1;
 // meaning: "পানি";
@@ -33,32 +86,39 @@ const wordLoad = (id) => {
 const displayWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
-  if(words.length==0){
+  if (words.length == 0) {
     wordContainer.innerHTML = `
     <div class="text-center mx-auto mt-10 p-16 col-span-3 space-y-3">
         <img class=" mx-auto" src="./assets/alert-error.png" alt="">
         <p class="font-bangla text-sm text[#79716B] ">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
         <h1 class="font-bold text-4xl text-[#292524]">নেক্সট Lesson এ যান।</h1>
        </div>
-    `;                          
+    `;
   }
   words.forEach((word) => {
-    const wordCard= document.createElement("div")
+    const wordCard = document.createElement("div");
     wordCard.innerHTML = `
          <div class="text-center mt-16 p-15 space-y-5 bg-white rounded-lg shadow-md">
-                <h1 class="font-bold  text-3xl">${word.word? word.word:"Word Not found"}</h1>
+                <h1 class="font-bold  text-3xl">${
+                  word.word ? word.word : "Word Not found"
+                }</h1>
                 <p class="font-medium text-xl"> Meaning/pronunciation </p>
-                <h1 class="font-semibold text-3xl text-[#18181B]">${word.meaning? word.meaning:"Meaning Not Found"}/${word.pronunciation? word.pronunciation: "Pronunciation Not Found"}</h1>
+                <h1 class="font-semibold text-3xl text-[#18181B]">${
+                  word.meaning ? word.meaning : "Meaning Not Found"
+                }/${
+      word.pronunciation ? word.pronunciation : "Pronunciation Not Found"
+    }</h1>
                 <div class="flex justify-between mt-9">
-                    <button class=" bg-[#1A91FF10] hover:bg-[#1A91FF80] p-4 rounded-sm"><i class="fa-solid fa-circle-info"></i></button>
+                    <button onclick="loadWordDetails(${
+                      word.id
+                    })" class=" bg-[#1A91FF10] hover:bg-[#1A91FF80] p-4 rounded-sm"><i class="fa-solid fa-circle-info"></i></button>
                     <button class=" bg-[#1A91FF10] hover:bg-[#1A91FF80] p-4 rounded-sm"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
             </div>
     `;
     // console.log(word);
-     wordContainer.append(wordCard);
+    wordContainer.append(wordCard);
   });
- 
 };
 
 // lessong Load and lesson related other functionality here
@@ -68,7 +128,7 @@ const displayLesson = (lessons) => {
     // console.log(lesson)
     const lessonBtn = document.createElement("div");
     lessonBtn.innerHTML = `
-    <button id="lessonbtn${lesson.level_no}" onclick="wordLoad(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}</button>
+    <button id="lessonbtn${lesson.level_no}" onclick="wordLoad(${lesson.level_no})" class="btn btn-outline btn-primary lessongBtnRemove"><i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}</button>
     `;
     lessonContainer.append(lessonBtn);
   }
